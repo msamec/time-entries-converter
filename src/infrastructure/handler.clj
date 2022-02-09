a80082512ed5948312a04b644ecb6fd8a3f7cdb3
Marko Samec <marko@samec.dev>
2022-02-09 14:00:02 +0100

    refactor(integrant): simply usage of integrant

--- src/infrastructure/handler.clj
+++ src/infrastructure/handler.clj
@@ -1,6 +1,7 @@
 (ns infrastructure.handler
   (:require [reitit.ring :as ring]
             [presentation.http.controller.user-ctrl :as user-ctrl]
+            [integrant.core :as ig]
             [ring.middleware.session :refer [wrap-session]]
             [ring.middleware.reload :refer [wrap-reload]]
             [ring.middleware.oauth2 :refer [wrap-oauth2]]
@@ -10,7 +11,7 @@
             [infrastructure.wrap.wrap-auth :refer [wrap-auth]]
             [infrastructure.wrap.wrap-form-params :refer [wrap-form-params]]))
 
-(defn app [oauth2]
+(defmethod ig/init-key :infrastructure.handler/app [_ {:keys [oauth2]}]
   (->
    [["/login" {:get {:handler user-ctrl/login}}]
     ["/" {:get {:handler user-ctrl/index
@@ -23,7 +24,7 @@
                                    :middleware [wrap-auth wrap-user-id-from-jwt]}}]]
    ring/router
    ring/ring-handler
-   (wrap-oauth2 {:google oauth2})
+   (wrap-oauth2 oauth2)
    (wrap-reload)
    (wrap-params)
    (wrap-content-type {:mime-types {nil "text/html"}})
--- src/infrastructure/server.clj
+++ src/infrastructure/server.clj
@@ -1,8 +1,5 @@
 (ns infrastructure.server
   (:require [ring.adapter.jetty :as jetty]
-            [infrastructure.handler :as handler]
-            [infrastructure.database.credential :as credential-imp]
-            [domain.credential-repository :as credential-repository]
             [integrant.core :as ig]
             [aero.core :as aero]
             [clojure.java.io :refer [resource]]))
@@ -14,14 +11,8 @@
 (def config
   (aero/read-config (resource "config.edn")))
 
-(defmethod ig/init-key :adapter/jetty [_ {:keys [handler] :as opts}]
+(defmethod ig/init-key :infrastructure.server/jetty [_ {:keys [handler] :as opts}]
   (jetty/run-jetty handler (-> opts (dissoc handler) (assoc :join? false))))
-
-(defmethod ig/init-key :handler/run-app [_ {:keys [oauth2]}]
-  (handler/app oauth2))
-
-(defmethod ig/init-key :oauth2/google [_ credentials]
-  credentials)
 
 (defmethod ig/init-key :secrets [_ _]) ;TODO does this really have to be here?
 
