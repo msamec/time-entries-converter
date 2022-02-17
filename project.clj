84f7850a88d1eeafdb9855393c47112ca520270c
Marko Samec <marko@samec.dev>
2022-02-17 12:19:36 +0100

    feat: prepare for Heroku depolyment (#14)
    * chore(lein): introduce lein
    
    * refactor(config): use env instead of .secrets.edn
    
    * build(uberjar): fix issues with generating uberjar
    
    * build: change database env var name
    
    * fix(typo): fix typo in configuration
    
    * test(migrator): remove migrator config from config.edn
    
    * chore(config): get port from env

---
+++ project.clj
@@ -0,0 +1,13 @@
+(defproject my-stuff "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "https://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "https://www.eclipse.org/legal/epl-v10.html"}
+  :plugins [[lein-tools-deps "0.4.5"]]
+  :main ^:skip-aot infrastructure.server
+  :middleware     [lein-tools-deps.plugin/resolve-dependencies-with-deps-edn]
+  :uberjar-name  "app-standalone.jar"
+  :target-path "target/%s"
+  :lein-tools-deps/config {:config-files [:install :user :project]}
+  :min-lein-version "2.0.0"
+  :profiles {:uberjar {:aot :all}})
